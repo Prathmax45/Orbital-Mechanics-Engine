0976243517d6b94cfe74977bd728a5e999f24ef3
Prathmax45 <prathmeshkanure@gmail.com>
2025-07-11 12:22:27 +0530

    body of  objects

---
+++ body.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "vector_3D.h"
+using namespace std;
+
+class body
+{
+    double mass;
+    vector_3D velocity;
+    vector_3D acceleration;
+    
+        
+
+    
+
+
+
+
+};
+
+
+
+
